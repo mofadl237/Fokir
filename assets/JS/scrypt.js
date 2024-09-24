ae8e1036b8af77c302fbde345b4a383d5f662c61
mofadl237 <mofadl237@gmail.com>
2024-09-24 16:25:46 +0300

    Add Section Services & section Portfolio

--- assets/JS/scrypt.js
+++ assets/JS/scrypt.js
@@ -1,19 +1,20 @@
 var icon = document.querySelector("header .icon");
-        var links = document.querySelector("header nav ul.d-flex");
-        var up =document.querySelector(".UP");
-        icon.addEventListener("click", function () {
-            icon.classList.toggle("icons");
-            links.classList.toggle("action");
-            links.classList.toggle("left");
-        })
-        window.addEventListener('scroll', function () {
-            if (window.scrollY >= 300) {
-                up.style.opacity="1"
-            } else {
-                up.style.opacity="0"
-            }
-        });
-        up.addEventListener("click",()=>{
-            window.scrollTo({top:0});
-        })
-        console.log(window.scrollY);
+var links = document.querySelector("header nav ul.d-flex");
+var up = document.querySelector(".UP");
+icon.addEventListener("click", function () {
+  icon.classList.toggle("icons");
+  links.classList.toggle("action");
+  links.classList.toggle("left");
+});
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 600) {
+    up.style.opacity = "1";
+  } else {
+    up.style.opacity = "0";
+  }
+});
+up.addEventListener("click", () => {
+  window.scrollTo({ top: 0 });
+});
+//Change Color In Style Using JS
+// document.documentElement.style.setProperty('--main-color',"#fc0");
